912bd364c967ea2eba0c0a311d3b7b82141c1401
tomoyama <gije.cho@gmail.com>
2016-01-09 16:49:49 +0000

    Implementation of CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # get 'messages/index'
   root 'messages#index'
   # resources :messages , only: [:create]
-  resources :messages
+  # resources :messages
+  resources :messages , except: [:index, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
