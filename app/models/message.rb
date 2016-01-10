72867db062a79fa2066b77895d3108dd64ea5b3d
tomoyama <gije.cho@gmail.com>
2016-01-10 08:14:59 +0000

    Modify message model by adding age

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+   # 内容は必須入力かつ1文字以上3文字以下
+  validates :age , length: { minimum: 1, maximum: 3 } , presence: true
 end
